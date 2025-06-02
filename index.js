2b996d8a588797f0dbb343372766e4c61202417c
elyasefrei <elyas.tarzet@efrei.net>
2025-06-02 22:14:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,15 +108,3 @@ document.querySelectorAll('.infos').forEach(infos => {
     tooltip.style.display = 'none';
 });
 });
-
-
-
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
-
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
-
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
