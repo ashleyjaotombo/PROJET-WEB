ece3b0ce08fd13803d12ccb586c62da88ef3a1d3
elyasefrei <elyas.tarzet@efrei.net>
2025-06-02 16:34:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ likes.forEach((like, index) => {
 
 
 //------------------------------------------------------------------------------------------------
-const articlesContainers = document.querySelectorAll(".articlefavoris");
+const articlesContainers = document.querySelectorAll(".articles");
 
-// Récupérer tous les favoris.css stockés
+// Récupérer tous les favoris stockés
 const savedItems = Object.values(localStorage);
 
 // Insérer les articles dans les blocs .articles (4 max chacun)
@@ -92,9 +92,30 @@ fermeButton.addEventListener("click", ()=>{
 });
 
 
+    document.querySelectorAll('.infos').forEach(infos => {
+    const tooltipId = infos.dataset.tooltipId;
+    const tooltip = document.getElementById(tooltipId);
+
+    infos.addEventListener('mouseenter', (e) => {
+    const rect = infos.getBoundingClientRect();
+    tooltip.style.top = `${rect.top + window.scrollY}px`;
+    tooltip.style.left = `${rect.right + 10}px`;
+    tooltip.style.display = 'block';
+});
+
+    infos.addEventListener('mouseleave', () => {
+    tooltip.style.display = 'none';
+});
+});
 
 
-//---------------------------------------------------------------------------------------
-const ajoutPanier=document.querySelectorAll()
 
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
