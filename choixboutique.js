50620e59f73827af6ff2d3ab9e98ce6ed8490db6
ashleyjaotombo <harinambinina.jaotombo-ashley@efrei.net>
2025-06-02 23:10:42 +0200

    Update choixboutique.js

--- choixboutique.js
+++ choixboutique.js
@@ -9,19 +9,3 @@ right.addEventListener('mouseenter', () => container.classList.add('hover-right'
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
 
 
-
-document.querySelectorAll('.infos').forEach(infos => {
-    const tooltipId = infos.dataset.tooltipId;
-    const tooltip = document.getElementById(tooltipId);
-
-    infos.addEventListener('mouseenter', (e) => {
-        const rect = infos.getBoundingClientRect();
-        tooltip.style.top = `${rect.top + window.scrollY}px`;
-        tooltip.style.left = `${rect.right + 10}px`;
-        tooltip.style.display = 'block';
-    });
-
-    infos.addEventListener('mouseleave', () => {
-        tooltip.style.display = 'none';
-    });
-});
